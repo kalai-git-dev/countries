9a7f67d28cac43ab6a6b881d7ce81239cc8bad6c
kalai-git-dev <mac@MacBook-Pro-de-mac.local>
2021-01-28 17:49:08 +0100

    sorted radio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,9 @@ function Home() {
   const [countries, setCountries] = useState([]);
   const [sortedCountries, setSortedCountries] = useState([]);
   const [dataFound, setDataFound] = useState(false);
+  const [rangeValue, setRangeValue] = useState(250);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "Europe", "Oceania", "Asia", "America"];
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,22 +22,63 @@ function Home() {
     if (dataFound) {
       const sortedCountry = () => {
         const countryObj = Object.keys(countries).map((i) => countries[i]);
-        console.log(countryObj);
+        // console.log(countryObj);
         const sortedArray = countryObj.sort(function (a, b) {
           return b.population - a.population;
         });
+        sortedArray.length = rangeValue;
         console.log(sortedArray);
         setSortedCountries(sortedArray);
       };
       sortedCountry();
     }
-  }, [dataFound]);
+  }, [dataFound, rangeValue]);
+  console.log(selectedRadio);
 
   return (
-    <div className="countries">
-      {sortedCountries.map((country) => {
-        return <Card key={country.name} country={country} />;
-      })}
+    <div>
+      <div>
+        <div>
+          <input
+            value={rangeValue}
+            type="range"
+            id="range"
+            min="1"
+            max="250"
+            onChange={(event) => {
+              setRangeValue(event.target.value);
+            }}
+          />
+          <label htmlFor="range">Countries :{rangeValue}</label>
+        </div>
+        <ul>
+          {radios.map((radio, index) => {
+            return (
+              <li key={index}>
+                <input
+                  type="radio"
+                  name="radio"
+                  id={radio}
+                  value={radio}
+                  checked={radio === selectedRadio}
+                  onChange={(event) => {
+                    setSelectedRadio(event.target.value);
+                  }}
+                />
+                <label htmlFor={radio}>{radio}</label>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="countries">
+        {sortedCountries
+          .filter((country) => country.region.includes(selectedRadio))
+          .map((country) => {
+            return <Card key={country.name} country={country} />;
+          })}
+      </div>
     </div>
   );
 }
