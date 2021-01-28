288cc67a89e35c2e93f465ddfc6ce6823c2e3e5d
kalai-git-dev <mac@MacBook-Pro-de-mac.local>
2021-01-28 06:09:37 +0100

    data sort

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,35 @@ import axios from "axios";
 import Card from "../components/Card";
 function Home() {
   const [countries, setCountries] = useState([]);
+  const [sortedCountries, setSortedCountries] = useState([]);
+  const [dataFound, setDataFound] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get("https://restcountries.eu/rest/v2/all");
-      console.log(response.data);
+      //   console.log(response.data);
       setCountries(response.data);
+      setDataFound(true);
     };
     fetchData();
-  }, []);
+
+    if (dataFound) {
+      const sortedCountry = () => {
+        const countryObj = Object.keys(countries).map((i) => countries[i]);
+        console.log(countryObj);
+        const sortedArray = countryObj.sort(function (a, b) {
+          return b.population - a.population;
+        });
+        console.log(sortedArray);
+        setSortedCountries(sortedArray);
+      };
+      sortedCountry();
+    }
+  }, [dataFound]);
 
   return (
     <div className="countries">
-      {countries.map((country) => {
+      {sortedCountries.map((country) => {
         return <Card key={country.name} country={country} />;
       })}
     </div>
